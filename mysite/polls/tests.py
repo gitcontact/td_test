72f881bc16a33997c867fd0f8890296b48dc7864
gitcontactme <gitcontactme@gmail.com>
2014-04-26 20:18:14 +0800

    fixed polls tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,10 +1,3 @@
-from django.db import models
-
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
-
-
 from django.test import TestCase
 from django.utils import timezone
 from polls.models import Poll
