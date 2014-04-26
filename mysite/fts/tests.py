cf362ecd4afe56876c595e545b8bd53a03736775
Sheng <webmaster0115@gmail.com>
2014-04-26 10:22:59 +0800

    added admin login test

--- mysite/fts/tests.py
+++ mysite/fts/tests.py
@@ -1,7 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class PollsTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,5 +20,22 @@ class PollsTest(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
-        # TODO: use the admin site to create a Poll
-        # self.fail('finish this test')
+        # She types in her username and passwords and hits return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # She now sees a couple of hyperlink that says "Polls"
+        # polls_links = self.browser.find_elements_by_link_text('Polls')
+        # self.assertEquals(len(polls_links), 2)
+
+        # TODO: Gertrude uses the admin site to create a new Poll
+        # self.fail('todo: finish tests')
